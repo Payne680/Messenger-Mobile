ec3baf7951f43000038b34702017048951347344
PENN JR <pennjunior680@gmail.com>
2023-06-30 18:04:06 +0100

    Designing the chat ibterface

--- src/screens/UserListing/UserListing.tsx
+++ src/screens/UserListing/UserListing.tsx
@@ -1,11 +1,30 @@
 import React from 'react'
 import { View, Text } from 'react-native'
 import { styles } from './UserListing.styles';
+import { SwipeListView } from 'react-native-swipe-list-view';
 
-export default function UserListing() {
+export type users = {
+    name: string;
+    item: string;
+}
+const Data = [
+    { id: 1, name: 'Jack Hallow', image: '' },
+    { id: 2, name: 'Jack Smith', image: '' },
+]
+
+const UserListing = () => {
+    const renderItem = ({ item }) => {
+        return (
+            <View style={styles.container}>
+                <Text>{item.name}</Text>
+            </View>
+        );
+    }
     return (
-        <View style={styles.container}>
-            <Text>UserListing</Text>
+        <View>
+            <SwipeListView data={Data} renderItem={renderItem} />
         </View>
     )
 }
+
+export default UserListing;
