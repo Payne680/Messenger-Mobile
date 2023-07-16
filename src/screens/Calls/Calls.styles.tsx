120e1daaabfab470028a09a6080d9b7587f7059e
PENN JR <pennjunior680@gmail.com>
2023-07-16 22:50:15 +0100

    Updating the color on my calls screen

--- src/screens/Calls/Calls.styles.tsx
+++ src/screens/Calls/Calls.styles.tsx
@@ -1,9 +1,10 @@
 import { StyleSheet } from "react-native";
+import { colors } from "../../theme/colors";
 
 export const styles = StyleSheet.create({
     container: {
         display: 'flex',
-        backgroundColor: '#fff',
+        backgroundColor: colors.white,
         flex: 1,
     }
 });
